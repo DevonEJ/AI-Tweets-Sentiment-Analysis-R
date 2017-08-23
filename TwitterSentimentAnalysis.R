6bfe7c5749675c7cd60bbefdca9e59bdb14369a0
Devon <miss.edwards.joseph@gmail.com>
2017-08-23 20:29:04 +0100

    Completed letter cloud, begun processing for sentiment

--- TwitterSentimentAnalysis.R
+++ TwitterSentimentAnalysis.R
@@ -1,6 +1,5 @@
 # Clear Environment
-
-rm(list=ls())
+save(list = ls(all.names = TRUE), file = ".RData", envir = .GlobalEnv)
 
 # Set Wd
 setwd("C:/Projects/Projects")
@@ -22,9 +21,11 @@ library(ggplot2)
 library(tidyr)
 library(tidytext)
 library(dplyr)
+library(wordcloud2)
+library(RColorBrewer)
+library(htmlwidgets)
 
 # create an object "cred" that will save the authenticated object that we can use for later sessions
-
 
 consumer_key <- "ADD"
 consumer_secret <- "ADD"
@@ -35,19 +36,20 @@ cred <- setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_
 
 # Search for tweets 
 
-search.string <- "lloyds banking group"
-no.of.tweets <- 325
+search.string <- "artificial + intelligence" 
+no.of.tweets <- 2000
 
-tweets <- searchTwitter(search.string, n = no.of.tweets, lang="en")
-tweets
+tweets <- searchTwitter(search.string, n = no.of.tweets, lang = "en")
 class(tweets)
+tweets <- gsub("@\\w+", "", tweets)
+#save(tweets, file = "aitweets.RData") # Save those same tweets for reproducability if desired, or refresh each time code runs
 
 # Convert text to data frame, and then corpus of documents (1 tweet = 1 document)
 
 tweets.df <- twListToDF(tweets) 
 dim(tweets.df)
 
-tweet_docs <- VCorpus(VectorSource(tweets.df$text))
+tweets_docs <- VCorpus(VectorSource(tweets.df$text))
 
 # Process the data -  removing symbols, numbers, misleading words etc.
 
@@ -55,36 +57,42 @@ removeURL <- function(x) gsub("http[^[:space:]]*", "", x) # Will remove URLs fro
 
 removeNumPunct <- function(x) gsub("[^[:alpha:][:space:]]*", "", x) # Removes characters that aren't letters or spaces when called below
 
-tweets_clean <- tm_map(tweets_clean, content_transformer(removeNumPunct))
-tweets_clean <- tm_map(tweets_clean, removeWords, c(stopwords("english")))#, "via", "amp", "lloyds", "banking", "group", "RT", "bank", "m", "aside", "has", "to"))
+tweets_clean <- tm_map(tweets_docs, content_transformer(removeNumPunct))
 tweets_clean <- tm_map(tweets_clean, content_transformer(removeURL))
-tweets_clean <- tm_map(tweet_docs, removePunctuation)
+tweets_clean <- tm_map(tweets_clean, removePunctuation)
 tweets_clean <- tm_map(tweets_clean, removeNumbers)
+tweets_clean <- tm_map(tweets_clean, content_transformer(tolower))
+tweets_clean <- tm_map(tweets_clean, removeWords, c(stopwords("english"), "via", "amp", "ai", "artificial", "intelligence", "rt", "artificialintelligence", "will", "ways"))
 tweets_clean <- tm_map(tweets_clean, stripWhitespace)
-
-
 
 # Create Term Document Matrix from clean corpus
 
 tweet.tdm <- TermDocumentMatrix(tweets_clean, control = list(wordLengths = c(1, Inf)))
 
-tweet.tdm
+# Which are the top 5 terms? Visualise with ggplot 2 barchart
 
-# Investigate most frequent terms in tdm
+freq.terms <- rowSums(as.matrix(tweet.tdm)) # Convert the TDM into a matrix first, then data frame to plot 
 
-freq.terms <- rowSums(as.matrix(tweet.tdm)) 
-freq.terms
+tweets.plot <- data.frame(term = names(freq.terms), freq = freq.terms)
 
-df.terms <- data.frame(term = names(freq.terms), freq = freq.terms)
-head(df.terms)
-
-# Visualise top terms with counts
-
-df.terms %>%
+tweets.df %>%
   arrange(freq) %>%
-  top_n(10) %>%
-  ggplot(aes(x = term, y = freq)) +
+  top_n(5) %>%
+  ggplot(aes(x = term, y = freq, fill = term)) +
   geom_bar(stat = "identity") +
   xlab("Terms") +
   ylab("Counts") +
+  scale_fill_brewer(palette = "Set1") +
   coord_flip()
+
+# 'Letter cloud' of frequent (> 15 freq) terms in shape 'AI'
+# HTML object created
+
+tweets.df %>%
+  filter(freq > 15) %>%
+  letterCloud(word = "AI", size = 1, color = "black")
+
+# Sentiment analysis with tidytext - How do people feel about AI in these tweets?
+# Tokenise tweets into single words
+
+tweets.tokens <- unnest_tokens(tweets.df, term, word) ##UNFINISHED
